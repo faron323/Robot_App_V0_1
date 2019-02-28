65c1d35a273fe75a48e22907495850ccc11d1e6c
phillip <47199407+phillipili@users.noreply.github.com>
2019-02-28 15:41:56 +1100

    kinematics

--- bluefruit/arm_joystick_control/arm_joystick_control.ino
+++ bluefruit/arm_joystick_control/arm_joystick_control.ino
@@ -1,86 +1,122 @@
+#include <Servo.h>
+
+Servo gripper_servo;
 
 //Joint Num:           0   1   2   3   4
-int pul_pin[] = {3,  4,  5,  6,  7};
-int dir_pin[] = {23, 25, 27, 29, 31};
+const int pul_pin[] = {3,  4,  5,  6,  7};
+const int dir_pin[] = {23, 25, 27, 29, 31};
 const int ena_pin[] = {22, 24, 26, 28, 30};
 
-int speed_initial[5] = {3000, 4000, 2000,  3000, 3000};
-int speed_target[5] =  {800,  800, 800,   3000, 3000};
-int acceleration[5] =  {15,    15,    15,     15,    15};
+int speed_final[5] =   {500, 1000, 200,  1000, 1000};
+int acceleration[5] =  {10,   15,   20,   15,   15};
+bool lock_gripper = false;
 
 void setup() {
-  attachInterrupt(digitalPinToInterrupt(21), interrupt_button_handler, LOW);
-  for(int i=0;i<5;i++){
-    pinMode(pul_pin[i], OUTPUT);
-    pinMode(dir_pin[i], OUTPUT); 
-    pinMode(ena_pin[i], OUTPUT);
-    digitalWrite(ena_pin[i], LOW);
-  }
-//  Serial.begin(9600);
+		//Init Controller Button Interrupts
+		for(int i=0; i<3; i++) {
+				pinMode(19+i, INPUT);
+				digitalWrite(19+i, HIGH);
+				attachInterrupt(digitalPinToInterrupt(19+i), interrupt_toggle_joint, HIGH);
+		}
+		//Init Motor Driver Outputs
+		for(int i=0; i<5; i++) {
+				pinMode(pul_pin[i], OUTPUT);
+				pinMode(dir_pin[i], OUTPUT);
+				pinMode(ena_pin[i], OUTPUT);
+				digitalWrite(ena_pin[i], LOW);
+		}
+		//Init Gripper Servo
+		gripper_servo.attach(9);
+		Serial.begin(2000000);
 }
 
 void loop() {
-  int js_read[5] = {};
-  int speed_current[5] = {};
-  int speed_joystick[5] = {};
-  int step_counter[5] = {};
-  long current_micros[5] = {}; 
-  long previous_micros[5] = {};
-  bool toggle[5] = {};
+		bool toggle[5] = {};
+		int servo_angle = 0;
+		int js_read[6] = {};
+		int step_counter[5] = {};
+		int speed_current[5] = {};
+		int speed_initial[5] = {};
+		int speed_joystick[5] = {};
+		long current_micros[5] = {};
+		long previous_micros[5] = {};
+		long current_millis, previous_millis = 0;
+		int servo_refresh_rate = 15;
 
-  while(1){
-    for(int i=0;i<5;i++){
-      js_read[i] = analogRead(i);
-      if (js_read[i]< 512){
-        digitalWrite(dir_pin[i], 0);
-      }else{
-        digitalWrite(dir_pin[i], 1);
-      }
-      speed_joystick[i] = abs(js_read[i]-512)*4;
-      
-      if (js_read[i] >530 or js_read[i]<494){
-        current_micros[i] = micros();
-        
-        if (current_micros[i] - previous_micros[i] >= speed_initial[i] - speed_current[i]){
-          toggle[i] = !toggle[i];
-          digitalWrite(pul_pin[i], toggle[i]);
 
-          if (speed_current[i] < speed_joystick[i]){
-            speed_current[i] += acceleration[i]; 
-          } else if (speed_current[i] > speed_joystick[i]){
-            speed_current[i] = speed_joystick[i];
-          }
-          //max speed current = 2048
-          //ramp up to 2048
-          
-          if (js_read[i]< 512){
-            step_counter[i]--;
-          }else{
-            step_counter[i]++;
-          }
-          previous_micros[i] = current_micros[i];
-        } 
-      }
-    }
-  }
+		for(int i=0; i<5; i++) {
+				speed_initial[i] = speed_final[i] + 2048;
+		}
+
+		while(1) {
+
+				for(int i=0; i<6; i++) {
+						js_read[i] = analogRead(5-i);
+						if (js_read[i] >530 or js_read[i]<494) {
+								if (js_read[i]< 512) {
+										digitalWrite(dir_pin[i], 0);
+								}else{
+										digitalWrite(dir_pin[i], 1);
+								}
+//        Serial.println(i);
+								speed_joystick[i] = abs(js_read[i]-512)*4;
+								current_micros[i] = micros();
+
+								if (current_micros[i] - previous_micros[i] >= speed_initial[i] - speed_current[i]) {
+
+										toggle[i] = !toggle[i];
+										digitalWrite(pul_pin[i], toggle[i]);
+
+										if (speed_current[i] < speed_joystick[i]) {
+												speed_current[i] += acceleration[i];
+										} else if (speed_current[i] > speed_joystick[i]) {
+												speed_current[i] = speed_joystick[i];
+										}
+										if (js_read[i]< 512) {
+												step_counter[i]--;
+										}else{
+												step_counter[i]++;
+										}
+
+										previous_micros[i] = current_micros[i];
+								}
+						}
+						if (i == 5) {
+								current_millis = millis();
+								if (current_millis - previous_millis >= servo_refresh_rate) {
+										if(lock_gripper == false) {
+												servo_angle = map(js_read[5], 0, 1023, 110, 20);
+												gripper_servo.write(servo_angle);
+												Serial.println(servo_angle);
+												previous_millis = current_millis;
+										}
+								}
+						}
+				}
+		}
 }
 
-void interrupt_button_handler(){
-  static unsigned int interrupt_previous_millis = 0;
-  static bool state = false;
-  unsigned int interrupt_millis = millis();
-  
-  if(interrupt_millis - interrupt_previous_millis > 200){  
-    if (state==true){
-      pul_pin[2] = 7;
-      dir_pin[2] = 31;
-      state == false;
-    }else{
-      pul_pin[2] = 5;
-      dir_pin[2] = 27;
-      state == true;
-    }
-    state = !state;
-  }
-  interrupt_previous_millis = interrupt_millis;
+void interrupt_toggle_joint(){
+		static unsigned int interrupt_previous_millis = 0;
+		unsigned int interrupt_millis = millis();
+
+		if(interrupt_millis - interrupt_previous_millis > 300) {
+				for(int i=0; i<3; i++) {
+						if (digitalRead(19+i) == 0) {
+								switch(i) {
+								case 0:
+//            Serial.println(i);
+										lock_gripper = !lock_gripper;
+										break;
+								case 1:
+										Serial.println(i);
+										break;
+								default:
+										Serial.println(i);
+										break;
+								}
+						}
+				}
+		}
+		interrupt_previous_millis = interrupt_millis;
 }
--- bluefruit/arm_step_control/arm_step_control.ino
+++ bluefruit/arm_step_control/arm_step_control.ino
@@ -3,91 +3,87 @@ const int pul_pin[] = {3,  4,  5,  6,  7};
 const int dir_pin[] = {23, 25, 27, 29, 31};
 const int ena_pin[] = {22, 24, 26, 28, 30};
 
-#define pul_pin_1_right 4
-#define dir_pin_1_right 25
-#define ena_pin_1_right 24 
-
-#define xaxis 97
-#define yaxis 96
-
-//Other motor driver in Joint 1 motor pair (currently being used to control motor 1)
-//4,25,24 (pulse, direction, enable)
-
-//Pins for motor 0 Driver (currently disabled because driver connection needs repairs)
-//2,21,20
-
 //Motor Control Variables
 //Joint Num:            0     1     2      3     4
-int steps[5] =         {1000,    1000,  500,     500,   500};    //Number of Steps
-bool dir[5] =          {0,    1,    1,     1,    0};    //Direction (0:Clockwise, 1: Anti-CW)
+float degree[5] =      {0,    0,    0,     0,    0};
+bool dir[5] =          {1,    0,    1,     1,    0};    //Direction (0:Clockwise, 1: Anti-CW)
+
 int speed_initial[5] = {2500, 2500, 1000,  5000, 5000};
 int speed_target[5] =  {900,  1000,  300,   5000, 5000};
 int acceleration[5] =  {1,    1,    1,     1,    1};
-int val_x, val_y;
+
 bool stop = false;
 
-void setup() {
-  for(int i=0;i<5;i++){
-    pinMode(pul_pin[i], OUTPUT);
-    pinMode(dir_pin[i], OUTPUT); 
-    pinMode(ena_pin[i], OUTPUT);
-    digitalWrite(ena_pin[i], LOW);
-    Serial.begin(9600);
 
-  }
+float step_deg[5] = {0.08823529, 0.27, 0.072, 1.8, 0.38297872};
+int microstep[5] = {16,16,16,8,16};
+
+void setup() {
+		for(int i=0; i<5; i++) {
+				pinMode(pul_pin[i], OUTPUT);
+				pinMode(dir_pin[i], OUTPUT);
+				pinMode(ena_pin[i], OUTPUT);
+				digitalWrite(ena_pin[i], LOW);
+				Serial.begin(2000000);
+		}
 }
 
 void loop() {
-  while(stop == false){
-      move_joints(steps, dir, speed_initial, speed_target, acceleration);
-    stop = true;
-  }
+		while(stop == false) {
+				move_joints(dir, speed_initial, speed_target, acceleration);
+				stop = true;
+		}
 }
 
-void move_joints(int steps[5], bool dir[5], int speed_initial[5], int speed_target[5], int acceleration[5]){
-  long current_micros[5] = {}; 
-  long previous_micros[5] = {};
-  int speed_current[5] = {};
-  int step_counter[5] = {};
-  int sustain_steps[5] = {};
-  int ramp_steps[5] = {};
-  bool toggle[5] = {};
-  
-  for(int i=0;i<5;i++){
-    //Set Directions
-    digitalWrite(dir_pin[i], dir[i]);
+void move_joints(bool dir[5], int speed_initial[5], int speed_target[5], int acceleration[5]){
+		long current_micros[5] = {};
+		long previous_micros[5] = {};
+		int speed_current[5] = {};
+		int step_counter[5] = {};
+		int sustain_steps[5] = {};
+		int ramp_steps[5] = {};
+		bool toggle[5] = {};
+		float steps[5] = {};
 
-    //Calculate Ramping Envelope
-    speed_current[i] = speed_initial[i];
+		for(int i=0; i<5; i++) {
+				steps[i] = degree[i]/step_deg[i] * microstep[i];
+				Serial.println(steps[i]);
+		}
 
-    ramp_steps[i] = (speed_initial[i] - speed_target[i]) / acceleration[i];
-    sustain_steps[i] = steps[i] - (ramp_steps[i]*2);
-  
-    if(ramp_steps[i] >= steps[i]/2){
-      ramp_steps[i] = steps[i]/2-1;
-      sustain_steps[i] = steps[i]-ramp_steps[i]*2;
-    }
-  }
+		for(int i=0; i<5; i++) {
+				//Set Directions
+				digitalWrite(dir_pin[i], dir[i]);
 
-  while(1){
-    
-    for(int i=0;i<5;i++){ 
-      if (step_counter[i] < steps[i]){
-        current_micros[i] = micros(); 
-        if (current_micros[i] - previous_micros[i] >= speed_current[i]){
-          toggle[i] = !toggle[i];
-          digitalWrite(pul_pin[i], toggle[i]);
+				//Calculate Ramping Envelope
+				speed_current[i] = speed_initial[i];
 
-          if(step_counter[i] < ramp_steps[i]){
-            speed_current[i] -= acceleration[i];
-          }else if((step_counter[i] >= (ramp_steps[i] + sustain_steps[i])-1)){
-            speed_current[i] += acceleration[i];
-          }
-          
-          step_counter[i]++;
-          previous_micros[i] = current_micros[i];
-        }
-      }
-    }
-  }
+				ramp_steps[i] = (speed_initial[i] - speed_target[i]) / acceleration[i];
+				sustain_steps[i] = steps[i] - (ramp_steps[i]*2);
+
+				if(ramp_steps[i] >= steps[i]/2) {
+						ramp_steps[i] = steps[i]/2-1;
+						sustain_steps[i] = steps[i]-ramp_steps[i]*2;
+				}
+		}
+
+		while(1) {
+
+				for(int i=0; i<5; i++) {
+						if (step_counter[i] < steps[i]) {
+								current_micros[i] = micros();
+								if (current_micros[i] - previous_micros[i] >= speed_current[i]) {
+										toggle[i] = !toggle[i];
+										digitalWrite(pul_pin[i], toggle[i]);
+
+										if(step_counter[i] < ramp_steps[i]) {
+												speed_current[i] -= acceleration[i];
+										}else if((step_counter[i] >= (ramp_steps[i] + sustain_steps[i])-1)) {
+												speed_current[i] += acceleration[i];
+										}
+										step_counter[i]++;
+										previous_micros[i] = current_micros[i];
+								}
+						}
+				}
+		}
 }
